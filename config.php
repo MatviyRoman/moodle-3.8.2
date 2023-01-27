a94052eac4693a1bfcce72000339a3d8c0647a85
Roman Matviy <roman@matviy.pp.ua>
2023-01-27 19:27:15 +0200

    moodle 3.8.2

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'm382';
+$CFG->dbuser    = 'user';
+$CFG->dbpass    = 'user';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://m382.ua';
+$CFG->dataroot  = 'D:\\programm\\OpenServer\\domains\\moodledataM382';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
